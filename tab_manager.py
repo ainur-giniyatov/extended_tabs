0fdab349bff25f001c20e4f7ab8a4f4ef2d61542
Ainur <anurique@gmail.com>
2019-08-11 23:55:49 +0500

    do not let withdraw a page if last one left

--- tab_manager.py
+++ tab_manager.py
@@ -1,5 +1,5 @@
 from PyQt5.QtCore import QObject, Qt, QPoint
-from PyQt5.QtWidgets import QMainWindow, QWidget
+from PyQt5.QtWidgets import QMainWindow, QWidget, QLabel
 from PyQt5.QtGui import QCursor
 
 from tab_widget import TabWidget
@@ -18,24 +18,26 @@ class TabManager(QObject):
         self._dragging_image = None
 
     def _detach_tab(self):
-        tab_widget = self._tab_bar.parent()
-        self._detached_widget = tab_widget.widget(self._index)
-        tab_widget.removeTab(self._index)
-        self._detached = True
-        print('detach')
+        if self._tab_bar.count() > 1:
+            tab_widget = self._tab_bar.parent()
+            self._detached_widget = tab_widget.widget(self._index)
+            self._detached_widget.setWindowTitle(tab_widget.tabText(self._index))
+            tab_widget.removeTab(self._index)
+            self._detached = True
 
     def _drop_tab(self, pos):
-        self._detached = False
-        if self._detached_widget:
+        if self._detached:
             new_window = QMainWindow(self._main_window)
             
             new_tab_widget = TabWidget(new_window)
             new_tab_widget.setTabManager(self)
             new_window.setCentralWidget(new_tab_widget)
-            new_tab_widget.addTab(self._detached_widget, 'tab')
+            new_tab_widget.addTab(self._detached_widget, self._detached_widget.windowTitle())
 
             new_window.show()
             new_window.move(QCursor.pos())
+
+        self._detached = False
             
     def left_pressed(self, pos, tab_bar):
         self._tab_bar = tab_bar
@@ -54,10 +56,15 @@ class TabManager(QObject):
         if self._away:
             if not self._detached:
                 self._detach_tab()
-                self._dragging_image = QWidget(None, Qt.FramelessWindowHint)
-                self._dragging_image.resize(50, 50)
-                self._dragging_image.show()
+                if self._detached:
+                    self._dragging_image = self._make_draggin_image()
         
         if self._dragging_image is not None:
             self._dragging_image.move(QCursor.pos() + QPoint(20, 20))
             
+    def _make_draggin_image(self):
+        dragging_image = QWidget(None, Qt.FramelessWindowHint)
+        label = QLabel(self._detached_widget.windowTitle(), self._dragging_image)
+        dragging_image.resize(50, 50)
+        dragging_image.show()
+        return dragging_image
