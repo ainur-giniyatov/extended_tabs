d46f39a900221955b7d0099882c5370b2ba1770b
Ainur <anurique@gmail.com>
2019-08-24 23:16:35 +0600

    moved a file to another place

--- src/extendedtabs/tab_manager.py
+++ src/extendedtabs/tab_manager.py
@@ -4,7 +4,7 @@ from PyQt5.QtCore import QObject, Qt, QPoint, QMimeData
 from PyQt5.QtWidgets import QMainWindow, QWidget, QLabel
 from PyQt5.QtGui import QCursor, QDrag, QPixmap
 
-from tab_drag import TabDrag
+from .tab_drag import TabDrag
 from .tab_widget import TabWidget
 
 
